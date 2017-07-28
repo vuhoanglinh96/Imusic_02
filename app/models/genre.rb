907808877da950bb578f95e48e7c4dc71c6dccd0
vuhoanglinh96 <linhvh.96@gmail.com>
2017-07-28 13:02:57 +0700

    render music in home page

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,8 @@ class Genre < ApplicationRecord
 
   validates :name, presence: :true,
     length: {maximum: Settings.validates.track.title.maximum}
+
+  def genres_collect
+    Genre.all.map{|genre| [genre.name, genre.id]}
+  end
 end
