8fc6a00b72f354223bdb8028becba05909e6bd90
vuhoanglinh96 <linhvh.96@gmail.com>
2017-07-31 12:26:18 +0700

    User config, db, model, js, css

--- app/assets/javascripts/js/audioObject.js
+++ app/assets/javascripts/js/audioObject.js
@@ -22,11 +22,21 @@ AudioObject.prototype.bindAudioPlayer = function (num) {
 }
 
 AudioObject.prototype.addEventListeners = function () {
-  this.audio.addEventListener("timeupdate", AudioObject.prototype.timeUpdate, false);
-  this.audio.addEventListener("durationchange", AudioObject.prototype.durationChange, false);
-  this.timeline.addEventListener("click", AudioObject.prototype.timelineClick, false);
-  this.playbutton.addEventListener("click", AudioObject.prototype.pressPlay, false);
-  this.playhead.addEventListener('mousedown', AudioObject.prototype.mouseDown, false);
+  this.audio.addEventListener(
+    "timeupdate", AudioObject.prototype.timeUpdate, false
+  );
+  this.audio.addEventListener(
+    "durationchange", AudioObject.prototype.durationChange, false
+  );
+  this.timeline.addEventListener(
+    "click", AudioObject.prototype.timelineClick, false
+  );
+  this.playbutton.addEventListener(
+    "click", AudioObject.prototype.pressPlay, false
+  );
+  this.playhead.addEventListener(
+    'mousedown', AudioObject.prototype.mouseDown, false
+  );
   window.addEventListener('mouseup', mouseUp, false);
 }
 
@@ -36,7 +46,8 @@ function populateAudioList() {
     audioList.push(
       new AudioObject(audioElements[i-1], 0)
     );
-    audioList[i-1].bindAudioPlayer(i);
+    num = audioList[i-1].id.replace("audio-", "")
+    audioList[i-1].bindAudioPlayer(num);
     audioList[i-1].addEventListeners();
   }
 }
@@ -79,22 +90,34 @@ AudioObject.prototype.play = function () {
 
 AudioObject.prototype.timelineClick = function (event) {
   var ao = audioList[getAudioListIndex(this.id)];
-  ao.audio.currentTime = ao.audio.duration * clickPercent(event, ao.timeline, ao.timelineWidth);
+  ao.audio.currentTime = ao.audio.duration * clickPercent(
+    event, ao.timeline, ao.timelineWidth
+  );
 }
 
 AudioObject.prototype.mouseDown = function (event) {
   onplayhead = this.id;
   var ao = audioList[getAudioListIndex(this.id)];
-  window.addEventListener('mousemove', AudioObject.prototype.moveplayhead, true);
-  ao.audio.removeEventListener('timeupdate', AudioObject.prototype.timeUpdate, false);
+  window.addEventListener(
+    'mousemove', AudioObject.prototype.moveplayhead, true
+  );
+  ao.audio.removeEventListener(
+    'timeupdate', AudioObject.prototype.timeUpdate, false
+  );
 }
 
 function mouseUp(e) {
   if (onplayhead != null) {
     var ao = audioList[getAudioListIndex(onplayhead)];
-    window.removeEventListener('mousemove', AudioObject.prototype.moveplayhead, true);
-    ao.audio.currentTime = ao.audio.duration * clickPercent(e, ao.timeline, ao.timelineWidth);
-    ao.audio.addEventListener('timeupdate', AudioObject.prototype.timeUpdate, false);
+    window.removeEventListener(
+      'mousemove', AudioObject.prototype.moveplayhead, true
+    );
+    ao.audio.currentTime = ao.audio.duration * clickPercent(
+      e, ao.timeline, ao.timelineWidth
+    );
+    ao.audio.addEventListener(
+      'timeupdate', AudioObject.prototype.timeUpdate, false
+    );
   }
   onplayhead = null;
 }
@@ -129,16 +152,19 @@ function printTime() {
   var audioElements = document.getElementsByClassName("audio");
   for (i = 1; i <= audioElements.length; i++) {
     var ao = audioList[i-1];
+    var num = audioList[i-1].id.replace("audio-", "");
     duration = parseInt(ao.audio.duration);
     var d = moment.duration(duration, 'seconds');
     var hours = Math.floor(d.asHours());
     var mins = Math.floor(d.asMinutes()) - hours * 60;
     var secs = Math.floor(d.asSeconds()) - hours * 3600 - mins * 60;
     if (hours != 0) {
-      document.getElementById("stop-time-" + i).innerHTML = hours + ':' + mins + ':' + secs;
+      document.getElementById("stop-time-" + num).innerHTML =
+        hours + ':' + mins + ':' + secs;
     }
     else
-      document.getElementById("stop-time-" + i).innerHTML = mins + ':' + secs;
+      document.getElementById("stop-time-" + num).innerHTML =
+        mins + ':' + secs;
   }
 }
 
@@ -146,16 +172,19 @@ function currentPlay() {
   var audioElements = document.getElementsByClassName("audio");
   for (i = 1; i <= audioElements.length; i++) {
     var ao = audioList[i-1];
+    var num = audioList[i-1].id.replace("audio-", "");
     duration = parseInt(ao.audio.currentTime);
     var d = moment.duration(duration, 'seconds');
     var hours = Math.floor(d.asHours());
     var mins = Math.floor(d.asMinutes()) - hours * 60;
     var secs = Math.floor(d.asSeconds()) - hours * 3600 - mins * 60;
     if (hours != 0) {
-      document.getElementById("start-time-" + i).innerHTML = hours + ':' + mins + ':' + secs;
+      document.getElementById("start-time-" + num).innerHTML =
+        hours + ':' + mins + ':' + secs;
     }
     else
-      document.getElementById("start-time-" + i).innerHTML = mins + ':' + secs;
+      document.getElementById("start-time-" + num).innerHTML =
+        mins + ':' + secs;
   }
 }
 
